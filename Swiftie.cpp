cf0a59b783449c238f06a904e99a062717963538
TheRealEan <imccurdy001@student.franciscan.edu>
2025-02-12 22:20:29 -0500

    Delete Swiftie.cpp

--- Swiftie.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: "Swiftie.cpp"
-#include "Swiftie.h"
-using namespace std;
-
-Swiftie::Swiftie()
-	: Swiftie("Swiftie", 70, 70, 70, 10, 10) { };
-
-Swiftie::Swiftie(string name, int health, int stamina, int mana, int strength, int armor)
-	: Enemy(name, health, stamina, mana, strength, armor) {};

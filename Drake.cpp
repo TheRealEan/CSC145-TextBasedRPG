fa12c942d4952bac1c5798369909783461094558
TheRealEan <imccurdy001@student.franciscan.edu>
2025-02-12 22:18:23 -0500

    Delete Drake.cpp

--- Drake.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: "Drake.cpp"
-#include "Drake.h"
-using namespace std;
-
-Drake::Drake(string name)
-	: Drake(name, 70, 70, 70, 10, 10) {};
-
-Drake::Drake(string name, int health, int stamina, int mana, int strength, int armor)
-	: Enemy(name, health, stamina, mana, strength, armor) {};

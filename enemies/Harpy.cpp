21a1faaded3ede1b9e229c2c70899707b2588f06
TheRealEan <imccurdy001@student.franciscan.edu>
2025-02-12 22:14:38 -0500

    Delete enemies directory

--- enemies/Harpy.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: "Harpy.cpp"
-#include "Harpy.h"
-using namespace std;
-
-Harpy::Harpy(string name)
-	: Harpy(name, 70, 70, 70, 10, 10) {};
-
-Harpy::Harpy(string name, int health, int stamina, int mana, int strength, int armor)
-	: Enemy(name, health, stamina, mana, strength, armor) {};
--- enemies/Roomba.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: "Roomba.cpp"
-#include "Roomba.h"
-using namespace std;
-
-Roomba::Roomba(string name)
-	: Roomba(name, 70, 70, 70, 10, 10) {};
-
-Roomba::Roomba(string name, int health, int stamina, int mana, int strength, int armor)
-	: Enemy(name, health, stamina, mana, strength, armor) {};

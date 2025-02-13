d0360cc8a51435b440efff9435b5e4089538efc7
TheRealEan <imccurdy001@student.franciscan.edu>
2025-02-12 22:20:49 -0500

    Delete TeaKettle.cpp

--- TeaKettle.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: "TeaKettle.cpp"
-#include "TeaKettle.h"
-using namespace std;
-
-TeaKettle::TeaKettle(string name)
-	: TeaKettle(name, 70, 70, 70, 10, 10) {};
-
-TeaKettle::TeaKettle(string name, int health, int stamina, int mana, int strength, int armor)
-	: Enemy(name, health, stamina, mana, strength, armor) {};
